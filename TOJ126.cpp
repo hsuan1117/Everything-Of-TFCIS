603a45dc6d37c2fe8500ff78eb4ea578b84eecd5
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2019-12-27 22:44:00 +0800

    Update TOJ126.cpp

--- TOJ126.cpp
+++ TOJ126.cpp
@@ -1,37 +1,22 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define maxN 10000
-#define maxS 10000
-int sco[maxN]; // 所有配分 
-int pos[maxS], r=0; 
-void gen (int m, int s){
-//m:= 現在己經選了 m 題; s:= 現在的累積分數     
-    if (m==0){
-        if(s>=0)pos[r++] = s; 
-        return;
-    }     
-    gen(m-1,s+sco[m-1]);
-    gen(m-1,s-sco[m-1]); 
-}
+set<int> s[105]; 
+
 int main() {
-    int scoNum,askNum;
-    cin >> scoNum >> askNum ;
-    for(int i=0;i<scoNum;i++){
-        cin >> sco[i];
-        
+    int n,q; 
+    cin>>n>>q; 
+    s[0].insert(0);
+    for(int i=0,t;i<n;i++){ 
+        cin>>t;
+        for(auto x:s[i]){ 
+            s[i+1].insert(x+t); 
+            s[i+1].insert(x-t); 
+        } 
     }
-    gen(scoNum ,0);
-    /*
-    cout << "=======DEBUG========" << endl;
-    for(int i=0;i<r;i++){
-        cout << pos[i] << endl; 
-    }
-    cout << "=======DEBUG========" << endl;*/
-    
-    for(int i=0;i<askNum;i++){
-        int data;cin >> data;
-        sort(begin(pos),begin(pos)+r);
-        cout << *lower_bound(begin(pos),begin(pos)+r,data)<< endl;
+    int t;
+    while(q--){
+      cin >> t;
+      cout << *s[n].lower_bound(t) << endl;
     }
     return 0;
 }
